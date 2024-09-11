f58586c6ccd6cdde342f12b85158ddcdfb825e48
dhana <dhana@JUSTME>
2024-09-11 20:06:51 +1000

    Add project files.

---
+++ Todolist/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Todolist
+{
+    internal class Program
+    {
+        private static TaskManager taskManager = new TaskManager();
+        static void Main(string[] args)
+        {
+           
+
+            ShowMenu();
+            while (true)
+            {
+                // get user input
+                Console.WriteLine("Enter your choice(1-5): ");
+                string userInput = Console.ReadLine();
+
+                // handle user request
+                switch (userInput)
+                {
+                    case "1":
+                        
+                }
+                
+            }
+
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("\r\nWelcome to the To-Do List Application!\r\n\r\n1.Add a new task\r\n2.View all tasks\r\n3.Mark a task as completed\r\n4.Delete a task\r\n5.Exit\r\n");
+        }
+    }
+}
---
+++ Todolist/TaskManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todolist
+{
+    internal class TaskManager
+    {
+        public AddTask()
+        {
+
+        }
+
+        public ViewTask()
+        {
+
+        }
+        public MarkTaskCompleted()
+        {
+
+        }
+        public DeleteTask()
+        {
+
+        }
+    }
+}
